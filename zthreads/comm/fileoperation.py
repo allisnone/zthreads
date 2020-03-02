2a58b8437c1c81b73f90ff7ec74c43780d7244e1
allisnone <zgx20022002@163.com>
2020-03-02 12:47:05 +0800

    update file operation

--- zthreads/comm/fileoperation.py
+++ zthreads/comm/fileoperation.py
@@ -106,3 +106,36 @@ def read_csv_list(file='./raw/users.csv',ignore_header=True,encoding='utf-8',log
                     print('Line {0} was empty line.'.format(i))
             i = i + 1
     return data,header
+
+
+def copyFiles(sourceDir,  targetDir): 
+    if sourceDir.find(".svn") > 0: 
+        return 
+    for file in os.listdir(sourceDir): 
+        sourceFile = os.path.join(sourceDir,  file) 
+        targetFile = os.path.join(targetDir,  file) 
+        if os.path.isfile(sourceFile): 
+            if not os.path.exists(targetDir):  
+                os.makedirs(targetDir)  
+            if not os.path.exists(targetFile) or(os.path.exists(targetFile) and (os.path.getsize(targetFile) != os.path.getsize(sourceFile))):  
+                    open(targetFile, "wb").write(open(sourceFile, "rb").read()) 
+        if os.path.isdir(sourceFile): 
+            First_Directory = False 
+            copyFiles(sourceFile, targetFile)
+            
+def coverFiles(sourceDir,  targetDir): 
+    for file in os.listdir(sourceDir): 
+        sourceFile = os.path.join(sourceDir,  file) 
+        targetFile = os.path.join(targetDir,  file) 
+        #cover the files 
+        if os.path.isfile(sourceFile): 
+            open(targetFile, "wb").write(open(sourceFile, "rb").read())
+            
+def removeFileInFirstDir(targetDir): 
+    for file in os.listdir(targetDir): 
+        targetFile = os.path.join(targetDir,  file) 
+        if os.path.isfile(targetFile): 
+            os.remove(targetFile)
+
+def moveFileto(sourceDir,  targetDir):      
+    shutil.copy(sourceDir,  targetDir)
