27964d06061d839dd5543a3e8043bb38759212ad
allisnone <zgx20022002@163.com>
2020-03-02 13:42:38 +0800

    update remove_dir_or_file

--- zthreads/comm/fileoperation.py
+++ zthreads/comm/fileoperation.py
@@ -3,6 +3,7 @@
 import csv
 import random
 import os,sys,time
+import shutil
 
 def write2csv(data,file='result.csv',encoding='utf-8',logger=None,clear=False):
     """
@@ -107,6 +108,57 @@ def read_csv_list(file='./raw/users.csv',ignore_header=True,encoding='utf-8',log
             i = i + 1
     return data,header
 
+def remove_dir(top_dir, pathname, type='dir',remove_all=True):
+    """
+    遍历指定文件夹中，删除指定的文件或目录名称，非绝对路径。
+    :param top_dir: str, given DIR
+    :param pathname: str, file name or dir name, or "*"
+    :param type: str, type="dir" or type="file"
+    :return: bool, if True, delete the existing file or dir
+    """
+    if os.path.exists(top_dir)==False:
+        print("top_dir not exists")
+        return False
+    if os.path.isdir(top_dir)==False:
+        print("top_dir not a dir")
+        return False 
+    is_pathname_exist = False
+    for dir_path,subpaths,files in os.walk(top_dir,False):
+        #print(dir_path,subpaths,files)
+        if type=='dir':
+            if os.path.split(dir_path)[-1]==pathname:
+                is_pathname_exist = True
+                for file in files:
+                    file_path=os.path.join(dir_path,file)
+                    print("delete file:%s"  %file_path)
+                    os.remove(file_path)
+                if subpaths:
+                    for path in subpaths:
+                        shutil.rmtree(os.path.join(dir_path,path))
+                    print("delete subpaths:%s" %subpaths)
+                print("delete dir:%s" %dir_path)
+                os.rmdir(dir_path)
+                if not remove_all and is_pathname_exist:
+                    break
+            else:
+                pass
+        elif type=='file':
+            for file in files:
+                if file==pathname:
+                    is_pathname_exist = True
+                    file_path=os.path.join(dir_path,file)
+                    print("delete file:%s"  %file_path)
+                    os.remove(file_path)
+                    if not remove_all and is_pathname_exist:
+                        break
+                else:
+                    pass
+        else:
+            print('Not exist type')
+            return False
+        if not remove_all and is_pathname_exist:
+            break
+    return is_pathname_exist
 
 def copyFiles(sourceDir,  targetDir): 
     if sourceDir.find(".svn") > 0: 
@@ -140,3 +192,6 @@ def removeFileInFirstDir(targetDir):
 
 def moveFileto(sourceDir,  targetDir):      
     shutil.copy(sourceDir,  targetDir)
+    
+    
+#remove_dir(top_dir='C:\\Users\\zhangguoxin\\git\\zthreads', pathname='test2', type='dir', remove_all=True)
